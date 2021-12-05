a179518d33728a95242737434b037924486e8da5
Reiven Cabate <reivenjimmuelfcabate@iskolarngbayan.pup.edu.ph>
2021-12-06 00:21:47 +0800

    Second commit on 1stProgram. Fixed the error of not getting input again after answering yes and edited the overall program.

--- 1stProgram.py
+++ 1stProgram.py
@@ -1,44 +1,44 @@
-# Lottery
-# Create a program that ask 3 numbers (0-9) from the user
-# Generate 3 random winning numbers (0-9)
-# Display "Winner" if all 3 input numbers matched the generated numbers
-# Display "You loss" if not
-# Display "Try again y/n" after each game
-# If the user enter "y" the user will play again
-# If "n" the program will exit
-
-print('Test your Luck in our Lottery')
-
 import random
 
-def generateThreeNumbers():
-    numbers = []
+def gameFunc():
+
+    # Get user 3 guesses of numbers from 0-9
+    userN = []
+    userGuesses = 0
+    while userGuesses < 3:
+        userN.append(int(input('Enter your number from 0-9: ')))
+        userGuesses = userGuesses + 1
+
+    # Generate 3 random numbers from 0-9
+    generatedN = []
     count = 0
     while count < 3:
-        numbers.append(random.randint(0, 10))
+        generatedN.append(random.randint(0, 10))
         count = count + 1
-    return numbers
 
-def getUserInput():
-    numbers = []
-    count = 0
-    while count < 3:
-        numbers.append(int(input('Enter your number from 0-9: ')))
-        count = count + 1
-    return numbers
+    # Check if the user's guess is within the generated numbers
+    # Return the total number of correct guesses
+    correctGuessN = 0
+    for n in userN:
+        if n in generatedN:
+            correctGuessN = correctGuessN + 1
+        else:
+            correctGuessN = correctGuessN 
+    return correctGuessN
 
-def displayOutput(winNumbersF, userNumbersF):
-    playAgain = 'y'
-    while playAgain[0] == 'y':
-        if winNumbersF == userNumbersF:
+def displayOutput():
+
+    # Evaluate if the user won the lottery or not
+    # Interactive loop to ask if the user want to try again or not
+    playAgain = 'Y'
+    while playAgain[0] == 'Y':
+        userScore = gameFunc()
+        if userScore == 3:
             print('Winner!')
         else:
             print('You lose!')
-        playAgain = input('Try again? (y or no): ')
+        playAgain = input('Try again? (y or n): ').upper()
 
-
-winningN = generateThreeNumbers()
-userN = getUserInput()
-displayOutput(winningN, userN)
-
+print('Test your luck in the Lottery Game!')
+displayOutput()
 print('Thank you for participating in our lottery!')
